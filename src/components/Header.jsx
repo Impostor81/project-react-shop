c38189af74711cced013b2ec5a934704f3dd2c11
Aleksandr Maltsev <0po4ta@gmail.com>
2022-02-12 00:26:22 +0200

    fixed links repo 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   <div className="nav-wrapper">
     <a href="#" className="logo">React Shop</a>
     <ul id="nav-mobile" className="right">
-      <li><a href="https://github.com/Impostor81/project-react-shop">Repo</a></li>
+      <li><a href="#">Repo</a></li>
     </ul>
   </div>
 </nav>;
